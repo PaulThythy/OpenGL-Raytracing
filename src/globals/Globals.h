74de21ec51b7523dc38a7dade9c01714f5113a41
PaulThythy <paul.thi@hotmail.fr>
2025-01-06 00:22:05 +0100

    Windows compatibility

--- src/globals/Globals.h
+++ src/globals/Globals.h
@@ -4,11 +4,11 @@
 #include <stdint.h>
 
 namespace Config {
-    inline int SAMPLES = 1;
-    inline int BOUNCES = 10;
+    static int SAMPLES = 1;
+    static int BOUNCES = 10;
 
-    inline uint32_t INIT_WINDOW_WIDTH   = 1920;
-    inline uint32_t INIT_WINDOW_HEIGHT  = 1080; 
+    static uint32_t INIT_WINDOW_WIDTH   = 1920;
+    static uint32_t INIT_WINDOW_HEIGHT  = 1080;
 }
 
 #endif
